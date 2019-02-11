1148e8e6101c8cfdce33ad5e2107d018eb83c1a3
AlexisLosenko <Losenko.alexis@gmail.com>
2019-02-11 14:55:08 +0100

    first commit

---
+++ Entrainement1/quatrieme/script.js
@@ -0,0 +1,9 @@
+function display(){
+	var name = document.getElementById('name').value;
+				
+var firstname = document.getElementById('firstname').value;
+				
+var city = document.getElementById('city').value;
+
+  alert('Nom : '+getElementById("name").value+' Prenom : '+getElementById("firstname").value+' Date de naissance: '+getElementById("city").value);
+}
