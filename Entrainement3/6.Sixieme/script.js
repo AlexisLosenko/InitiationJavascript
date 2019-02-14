44f36a25a206edb01add23c781251ccb7b82dd5c
AlexisLosenko <Losenko.alexis@gmail.com>
2019-02-14 11:27:27 +0100

    sixième dossier

---
+++ Entrainement3/6.Sixieme/script.js
@@ -0,0 +1,16 @@
+document.getElementById("afficher").onclick = function() {afficher()};
+let tab = [-2,1,4];
+
+function soustrait(x){
+    if (x>=2) {
+        return alert(x - 2);
+    }
+    else {
+        return alert("Nombre négatif !");
+    }
+}
+function afficher(){
+    soustrait(tab[0]);
+    soustrait(tab[1]);
+    soustrait(tab[2]);
+}
