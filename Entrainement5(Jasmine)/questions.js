5ae21cfae1267bfd8fa78c97fd7f8b9557f093f5
AlexisLosenko <Losenko.alexis@gmail.com>
2019-02-18 11:47:51 +0100

    correction

--- Entrainement5(Jasmine)/questions.js
+++ Entrainement5(Jasmine)/questions.js
@@ -1,8 +1,4 @@
-/**
- * Exercice sur les chaines de caractères.
- * Trouvez la façon de faire la plus optimal.
- * Il peut y avoir plusieur façon de faire.
- */
+
 var tailleString = function(texte) {
   return texte.length;
 };
@@ -16,22 +12,11 @@ var concatString = function(texte1, texte2) {
   return concated;
 };
 var afficherCar5 = function(texte) {
-  // split() divise la string et la stock dans un array. le paramètre "" premet de séparer chaque caractère (espace compris).
   let e = texte.split("");
   return e[4];
 };
 var afficher9Car = function(texte) {
-  // ÇA MARCHE MAIS C'EST LONG
-  //   let e = texte.split("", 9);
-  //   let a = "";
-  //   for (let i = 0; i < e.length; i++) {
-  //     a += e[i];
-  //   }
-  //   console.log(a);
-  //   return a;
-  // --------------------------------------------------------
 
-  // ÇA MARCHE EN 2 LIGNES 8)
   let e = texte.substring(0, 9);
   return e;
 };
@@ -47,7 +32,7 @@ var minusculeString = function(texte) {
 };
 
 var SupprEspaceString = function(texte) {
-  //   trim()SUPPRIME LES ESPACES SUPPERFLUX
+
   let e = texte.trim();
   return e;
   // -----------------------------------------------
@@ -59,15 +44,11 @@ var IsString = function(texte) {
 };
 
 var AfficherExtensionString = function(texte) {
-  // particularly files with no extension (.htaccess etc included).
-  // It's very performant, and handles corner cases in an arguably better way by returning "" instead of the full string when there's no dot or no string before the dot.
-  //  It's a very well crafted solution, albeit tough to read. Stick it in your helpers lib and just use it.
 
   return texte.substring(texte.lastIndexOf(".") + 1, texte.length) || texte;
 };
 
 var NombreEspaceString = function(texte) {
-  // ici split() séléctionne tous les mots sans les espaces. donc si on supprime le dernier mot (après lequel il n'y aura pas d'espace) grâce à length -1 on obtient le nombre d'espace
   let e = texte.split(" ").length - 1;
   return e;
 };
@@ -79,9 +60,7 @@ var InverseString = function(texte) {
   return e;
 };
 
-/**
- * Exercices sur les nombres et les caluls mathématiques
- */
+
 var calculPuissance = function(x, y) {
   let result = 0;
   for (let i = 1; i < y; i++) {
